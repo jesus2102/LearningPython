79456d8fe99d11fe8123725e62d62cd33b028599
jesus2102 <jesusperezmarin@gmail.com>
2019-05-15 19:44:13 +0100

    mathlibs

---
+++ mathlibs.py
@@ -0,0 +1,13 @@
+#MadLibs - Practice Inout and Output
+#
+#Template:
+#   I enjoy oractuce! I find it helps me to__(verb)__ better.
+#   Without practice, my __(noun)__ would probably not even work.
+#    My code is getting more __(adjetive)__ every single day!
+
+
+#TODO: Prompt the user for parts of speech and store them in variables
+
+
+
+#TODO: Output the template to the screen with the blanks filled out with what the user stated
